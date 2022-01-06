fa4224238a57c58a70b838a74b185178cfac59d2
Ricardo Salido <juniores95@hotmail.com>
2022-01-06 15:59:30 -0300

    DepartmentFormController

---
+++ workshop-javafx-jdbc/src/gui/DepartmentFormController.java
@@ -0,0 +1,52 @@
+package gui;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import gui.util.Constraints;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class DepartmentFormController implements Initializable{
+
+	@FXML
+	private TextField txtId;
+	
+	@FXML
+	private TextField TxtName;
+	
+	@FXML
+	private Label labelErrorName;
+	
+	@FXML 
+	private Button btSave;
+	
+	@FXML
+	private Button btCancel;
+	
+	@FXML
+	public void onBtSaveAction() {
+		System.out.println("onBtSaveAction");
+	}
+	
+	@FXML
+	public void onBtCancelAction() {
+		System.out.println("onBtCancelAction");
+	}
+	
+	
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	private void initializaNodes() {
+		Constraints.setTextFieldInteger(txtId);
+		Constraints.setTextFielMaxLength(TxtName, 30);
+	}
+
+}
