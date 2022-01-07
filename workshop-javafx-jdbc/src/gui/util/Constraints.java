e8d4eb3c7dffa441f942d617faf70fc15f723a8e
Ricardo Salido <juniores95@hotmail.com>
2022-01-07 14:49:45 -0300

    Validation exception

--- workshop-javafx-jdbc/src/gui/util/Constraints.java
+++ workshop-javafx-jdbc/src/gui/util/Constraints.java
@@ -6,7 +6,7 @@ public class Constraints {
 
 	public static void setTextFieldInteger(TextField txt) {
 		txt.textProperty().addListener((obs, oldValue, newValue) -> {
-			if (newValue != null && !newValue.matches("\\d")) {
+			if (newValue != null && !newValue.matches("\\d*")) {
 				txt.setText(oldValue);
 			}
 		});
